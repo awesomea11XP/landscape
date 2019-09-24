de6603e4254a5dd364eae99e0d56033a741cdf24
awesomea11XP <54687268+awesomea11XP@users.noreply.github.com>
2019-09-24 15:52:50 -0500

    Update landscape.py
    added koopa

--- landscape.py
+++ landscape.py
@@ -1,10 +1,25 @@
 import turtle
 
 luigi = turtle.Turtle()
+koopa = turtle.Turtle()
+
+khide = koopa.color("snow4")
 
 
 
-luigi.color("white")
+def roadlines(i):
+    for i in range (i):
+        koopa.color("gold")
+        koopa.forward(1)
+        koopa.color("snow4")
+        koopa.forward(2)
+        koopa.color("gold")
+        koopa.forward(2)
+
+
+
+turtle.bgcolor("midnight blue")
+luigi.color("midnight blue")
 luigi.right(90)
 luigi.forward(45)
 luigi.color("green")
@@ -20,9 +35,26 @@ luigi.forward(500)
 luigi.right(90)
 luigi.forward(500)
 luigi.end_fill()
-
-
-
+luigi.color("snow4")
+luigi.begin_fill()
+luigi.right(45)
+luigi.forward(500)
+luigi.right(135)
+luigi.forward(750)
+luigi.right(135)
+luigi.forward(500)
+luigi.right(45)
+luigi.forward(50)
+luigi.end_fill()
+koopa.color("midnight blue")
+koopa.right(90)
+koopa.forward(45)
+koopa.right(90)
+koopa.color("snow4")
+koopa.forward(25)
+koopa.right(270)
+koopa.speed(900)
+roadlines(100)
 
 
 
