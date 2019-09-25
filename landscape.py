a0974557134eb16e41f737857917875a9783653d
awesomea11XP <54687268+awesomea11XP@users.noreply.github.com>
2019-09-25 15:49:28 -0500

    Update landscape.py
    added mario. basic isn't working, hopefully can get that fixed

--- landscape.py
+++ landscape.py
@@ -1,20 +1,34 @@
 import turtle
 
+
+mario = turtle.Turtle
 luigi = turtle.Turtle()
 koopa = turtle.Turtle()
 
 khide = koopa.color("snow4")
 
 
+def basic(i):
+    for i in range (i):
+        mario.forward(i)
+        mario.right(270)
+        mario.forward(i+100)
+        mario.right(270)
+        mario.forward(i)
+        mario.right(270)
+        mario.forward(i+100)
+        mario.right(270)
+        mario.forward(i)
+
 
 def roadlines(i):
     for i in range (i):
         koopa.color("gold")
-        koopa.forward(1)
+        koopa.forward(i+1)
         koopa.color("snow4")
-        koopa.forward(2)
+        koopa.forward(i+1)
         koopa.color("gold")
-        koopa.forward(2)
+        koopa.pensize(i+1)
 
 
 
@@ -25,24 +39,24 @@ luigi.forward(45)
 luigi.color("green")
 luigi.begin_fill()
 luigi.right(270)
-luigi.forward(500)
-luigi.right(90)
-luigi.forward(500)
+luigi.forward(1000)
 luigi.right(90)
 luigi.forward(1000)
 luigi.right(90)
-luigi.forward(500)
+luigi.forward(2000)
 luigi.right(90)
-luigi.forward(500)
+luigi.forward(1000)
+luigi.right(90)
+luigi.forward(1000)
 luigi.end_fill()
 luigi.color("snow4")
 luigi.begin_fill()
 luigi.right(45)
-luigi.forward(500)
-luigi.right(135)
 luigi.forward(750)
 luigi.right(135)
-luigi.forward(500)
+luigi.forward(1100)
+luigi.right(135)
+luigi.forward(750)
 luigi.right(45)
 luigi.forward(50)
 luigi.end_fill()
@@ -54,8 +68,9 @@ koopa.color("snow4")
 koopa.forward(25)
 koopa.right(270)
 koopa.speed(900)
-roadlines(100)
+roadlines(20)
 
+basic(50)
 
 
 
