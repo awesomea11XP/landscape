871fe8005ff442c187698e75b09f6cfd2fcc70be
awesomea11XP <54687268+awesomea11XP@users.noreply.github.com>
2019-09-27 15:50:33 -0500

    Update landscape.py
    changed basic to spire

--- landscape.py
+++ landscape.py
@@ -1,22 +1,23 @@
 import turtle
 
 
-mario = turtle.Turtle
+mario = turtle.Turtle()
 luigi = turtle.Turtle()
 koopa = turtle.Turtle()
 
 khide = koopa.color("snow4")
 
 
-def basic(i):
+
+def spire(i):
     for i in range (i):
         mario.forward(i)
         mario.right(270)
-        mario.forward(i+100)
+        mario.forward(i+50)
         mario.right(270)
         mario.forward(i)
         mario.right(270)
-        mario.forward(i+100)
+        mario.forward(i+50)
         mario.right(270)
         mario.forward(i)
 
@@ -69,8 +70,65 @@ koopa.forward(25)
 koopa.right(270)
 koopa.speed(900)
 roadlines(20)
+mario.right(90)
+mario.color("midnight blue")
+mario.forward(45)
+mario.right(90)
+mario.color("snow4")
+mario.forward(45)
+mario.color("green")
+mario.forward(300)
+mario.color("black")
+mario.fillcolor("dim grey")
+mario.right(180)
+mario.begin_fill()
+mario.forward(50)
+mario.right(270)
+mario.forward(150)
+mario.right(270)
+mario.forward(100)
+mario.right(270)
+mario.forward(150)
+mario.right(270)
+mario.forward(50)
+mario.end_fill()
+mario.forward(100)
+mario.fillcolor("light grey")
+mario.begin_fill()
+mario.forward(50)
+mario.right(270)
+mario.forward(200)
+mario.right(315)
+mario.forward(100)
+mario.right(45)
+mario.right(180)
+mario.forward(270)
+mario.right(270)
+mario.forward(50)
+mario.end_fill()
+mario.forward(125)
+mario.fillcolor("light steel blue")
+mario.begin_fill()
+mario.forward(75)
+mario.right(270)
+mario.forward(76)
+mario.right(270)
+mario.forward(150)
+mario.right(270)
+mario.forward(76)
+mario.right(270)
+mario.forward(75)
+mario.end_fill()
+mario.forward(100)
+mario.fillcolor("grey")
+mario.begin_fill()
+mario.speed(10)
+spire(14)
+mario.end_fill()
 
-basic(50)
+
+
+
 
 
 
